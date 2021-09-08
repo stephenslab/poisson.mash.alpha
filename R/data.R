43e1d482b07cfc4fcf3d4067fff2427f1d00c6e7
Peter Carbonetto <peter.carbonetto@gmail.com>
2021-09-08 12:42:13 -0500

    Added help(sim_raw).

---
+++ R/data.R
@@ -0,0 +1,13 @@
+#' @name sim_raw
+#'
+#' @title Data Used in Simulations
+#'
+#' @description Description of data set goes here.
+#' 
+#' @docType data
+#' 
+#' @format More details about the data go here.
+#' 
+#' @keywords data
+#'
+NULL
